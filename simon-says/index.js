28647765f80a3d35e86796585099737c51317e87
ghanizadev <jf.melo6@gmail.com>
2024-11-23 01:46:57 +0200

    Fix record

--- simon-says/index.js
+++ simon-says/index.js
@@ -1,12 +1,12 @@
-const startButton = document.querySelector('#start-btn');
-const restartButton = document.querySelector('#restart-btn');
+const startButton = document.querySelector("#start-btn");
+const restartButton = document.querySelector("#restart-btn");
 
 const buttons = {
-    red: document.querySelector('[data-color="red"]'),
-    yellow: document.querySelector('[data-color="yellow"]'),
-    blue: document.querySelector('[data-color="blue"]'),
-    green: document.querySelector('[data-color="green"]'),
-}
+  red: document.querySelector('[data-color="red"]'),
+  yellow: document.querySelector('[data-color="yellow"]'),
+  blue: document.querySelector('[data-color="blue"]'),
+  green: document.querySelector('[data-color="green"]'),
+};
 
 let currentSequence = [];
 let guessedSequence = [];
@@ -15,118 +15,137 @@ let roundStarted = false;
 let canClick = true;
 
 async function waitForSeconds(seconds) {
-    return new Promise(res=> setTimeout(() => res(null), seconds * 1000));
+  return new Promise((res) => setTimeout(() => res(null), seconds * 1000));
 }
 
 async function blink(color) {
-    return new Promise(res => {
-        buttons[color].setAttribute('data-blink', '1');
+  return new Promise((res) => {
+    buttons[color].setAttribute("data-blink", "1");
 
-        setTimeout(() => {
-            buttons[color].removeAttribute('data-blink');
-            res(null);
-        }, 250);
-    })
+    setTimeout(() => {
+      buttons[color].removeAttribute("data-blink");
+      res(null);
+    }, 250);
+  });
 }
 
 function randomColor() {
-    const colors = ['red', 'yellow', 'green', 'blue'];
-    const index = Math.floor(Math.random() * 4);
-    return colors[index];
+  const colors = ["red", "yellow", "green", "blue"];
+  const index = Math.floor(Math.random() * 4);
+  return colors[index];
 }
 
 function addRound() {
-    const color = randomColor();
-    currentSequence.push(color);
+  const color = randomColor();
+  currentSequence.push(color);
 }
 
 async function failed(it = 0) {
-    const promises = [];
+  const promises = [];
 
-    for(const color of Object.keys(buttons)) {
-        promises.push(blink(color));
-    }
+  for (const color of Object.keys(buttons)) {
+    promises.push(blink(color));
+  }
 
-    await Promise.all(promises);
-    await waitForSeconds(0.3);
+  await Promise.all(promises);
+  await waitForSeconds(0.3);
 
-    if(it < 4) failed(it + 1);
+  if (it < 4) failed(it + 1);
 }
 
 async function playSequence() {
-    canClick = false;
+  canClick = false;
 
-    for(const color of currentSequence) {
-        await blink(color);
-        await waitForSeconds(0.1);
-    }
+  for (const color of currentSequence) {
+    await blink(color);
+    await waitForSeconds(0.1);
+  }
 
-    canClick = true;
+  canClick = true;
 }
 
 function updateUI() {
-    document.querySelector('#current-score').innerHTML = "Atual: " + currentSequence.length;
+  document.querySelector("#current-score").innerHTML =
+    "Atual: " + currentSequence.length;
 }
 
 async function makeGuess(color) {
-    if(!roundStarted || !canClick) return;
+  if (!roundStarted || !canClick) return;
 
-    await blink(color);
-    guessedSequence.push(color);
+  await blink(color);
+  guessedSequence.push(color);
 
-    for(let i = 0; i < currentSequence.length; i++) {
-        const correctColor = currentSequence[i];
-        
-        if(guessedSequence.length >= i + 1 && guessedSequence[i] !== correctColor) {
-            await restart();
-            return;
-        }
+  for (let i = 0; i < currentSequence.length; i++) {
+    const correctColor = currentSequence[i];
+
+    if (
+      guessedSequence.length >= i + 1 &&
+      guessedSequence[i] !== correctColor
+    ) {
+      await restart();
+      return;
     }
+  }
 
-    if(guessedSequence.length < currentSequence.length)
-        return;
+  if (guessedSequence.length < currentSequence.length) return;
 
-    guessedSequence = [];
-    await waitForSeconds(1);
-    updateUI();
-    addRound();
-    playSequence();
+  guessedSequence = [];
+  await waitForSeconds(1);
+  updateUI();
+  addRound();
+  playSequence();
 }
 
 function start() {
-    if(roundStarted) return;
+  if (roundStarted) return;
 
-    roundStarted = true;
-    addRound();
-    playSequence();
+  roundStarted = true;
+  addRound();
+  playSequence();
 }
 
 async function restart() {
-    canClick = false;
-    localStorage.setItem('current-record', (currentSequence.length - 1).toString());
-    document.querySelector('#current-record').innerHTML = "Recorde: " + (currentSequence.length - 1).toString();
-    document.querySelector('#current-score').innerHTML = "Atual: 0";
-    currentSequence = [];
-    guessedSequence = [];
+  canClick = false;
+  const currentScore = currentSequence.length - 1;
+  const record = parseInt(localStorage.getItem("current-record") ?? "0");
 
-    await failed();
-    roundStarted = false;
-    canClick = true;
+  if (currentScore > record) {
+    localStorage.setItem("current-record", currentScore);
+    document.querySelector("#current-record").innerHTML =
+      "Recorde: " + (currentSequence.length - 1).toString();
+  }
+
+  document.querySelector("#current-score").innerHTML = "Atual: 0";
+  currentSequence = [];
+  guessedSequence = [];
+
+  await failed();
+  roundStarted = false;
+  canClick = true;
 }
 
-(function() {
-    startButton.addEventListener('click', (e) => {
-        start();
-    });
-    
-    restartButton.addEventListener('click', (e) => {
-        restart();
-    });
+(function () {
+  startButton.addEventListener("click", (e) => {
+    start();
+  });
 
-    buttons.red.addEventListener('click', () => { makeGuess('red') });
-    buttons.yellow.addEventListener('click', () => { makeGuess('yellow') });
-    buttons.blue.addEventListener('click', () => { makeGuess('blue') });
-    buttons.green.addEventListener('click', () => { makeGuess('green') });
+  restartButton.addEventListener("click", (e) => {
+    restart();
+  });
 
-    document.querySelector('#current-record').innerHTML = "Recorde: " + (localStorage.getItem('current-record') ?? '0');
-})() 
+  buttons.red.addEventListener("click", () => {
+    makeGuess("red");
+  });
+  buttons.yellow.addEventListener("click", () => {
+    makeGuess("yellow");
+  });
+  buttons.blue.addEventListener("click", () => {
+    makeGuess("blue");
+  });
+  buttons.green.addEventListener("click", () => {
+    makeGuess("green");
+  });
+
+  document.querySelector("#current-record").innerHTML =
+    "Recorde: " + (localStorage.getItem("current-record") ?? "0");
+})();
